d93d6bf5214661d9f8de36d7d060babf89ed941b
Brillian Ayub Septianus <briancallvins@gmail.com>
2017-03-08 22:03:39 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html> <html>
+<head>
+<title>Login Administrator</title>
+<link rel="stylesheet" type="text/css" href="css/login.css"/>
+</head>
+<body>
+<div class="wrap">
+<div class="head">
+Login administrator
+</div>
+<div class="form">
+<form name="fform" action="aksi.php" method="post">
+Username : <br/>
+<input type="text" name="username"/>
+<br/>
+Password : <br/>
+<input type="password" name="password"/>
+<br/>
+<input type="submit" value="login" name="button"/>
+</form>
+</div>
+<div class="footer">
+COPYRIGHT 2015
+</div>
+</div>
+</body>
+</html>
