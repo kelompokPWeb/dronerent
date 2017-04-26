8dcff2d528d0b2415ca7e9f3dec3903f0f1bde64
IqbalMaolana30 <iqbalmaolana333@gmail.com>
2017-04-26 22:41:04 +0700

    Add files via upload

---
+++ ins.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" >
+</head>
+<title>Rental Drone Yogyakarta</title>
+
+<body>
+<div class="header">
+Rental Drone
+<div class="kop">
+Yogyakarta
+</div>
+</div>
+<div class="navigation">
+		<a href ="nota.php">DATA BOOKING</a>
+			<a href ="ins.php">FORM DATA</a>
+			<a href ="home.php">LOGOUT</a>
+</div>
+<html>  
+<head>  
+<style type="text/css">  
+.putih {  
+ color: #FFF;  
+}  
+</style>  
+</head>  
+<div class="content1">
+<body>  
+<div align="center">  
+  <form method="post" action="ins_sv.php">  
+    <p>&nbsp;</p>  
+    <table width="383" height="201" border="1">  
+      <tr>  
+        <td colspan="2" bgcolor="#666666"><div align="center" class="putih"><strong>RENTAL Drone Yogyaarta</strong></div></td>  
+      </tr>  
+      <tr>  
+        <td width="124" bgcolor="#CCCCCC">Foto </td>  
+        <td width="243" bgcolor="#CCCCCC">&nbsp;<input name="foto" type="file" /></td>  
+      </tr>  
+      <tr>  
+        <td bgcolor="#CCCCCC">Nama Drone</td>  
+        <td bgcolor="#CCCCCC">&nbsp;<input name="nama" type="text" /></td>  
+      </tr>  
+      <tr>  
+        <td bgcolor="#CCCCCC">Tarif</td>  
+        <td bgcolor="#CCCCCC">&nbsp;<input name="tarif" type="text" /></td>  
+      </tr>  
+      <tr>  
+        <td height="28" bgcolor="#CCCCCC">&nbsp;</td>  
+        <td bgcolor="#CCCCCC"><input name="Simpan" type="submit" value="Simpan" />&nbsp;<input name="" type="reset" value="Hapus"/>&nbsp;<a href="index1.php" type="submit">Batal</a></td >  
+	
+      </tr>  
+    </table>  
+  </form>  
+</div>  
+</div>  
+</body>  
+
+<div class="footer">
+<marquee>Copyright 2017</marquee>
+</div>
+
+</html>  
